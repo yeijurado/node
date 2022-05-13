e842f39f630d19ab1aaa12164fa7f1a1b7fbc5ca
Yeison Jurado <yjurado@bancolombia.com.co>
2022-05-13 18:24:17 -0500

    versión del punto 4 corregida y mejorada

--- YeisonJurado_Examen4.js
+++ YeisonJurado_Examen4.js
@@ -1,16 +1,18 @@
-
 class Persona {
     constructor (nombre, apellido, edad) {
-        this.nombre = nombre;
+        this._nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
     }
-    getDetalles() 
-{
-    console.log(Persona.nombre);
-    console.log(Persona.apellido);
-    console.log(Persona.edad);    
+    
+
+get nombre(){
+ return this._nombre;
 }
+
+set nombre(nuevoNombre){
+  this._nombre = nuevoNombre;
+ }
 }
 
 
@@ -18,19 +20,24 @@ class Persona {
 class Empleado extends Persona {
     constructor(nombre, apellido, edad, tipo) {
     super(nombre, apellido, edad);
-    this.tipo = tipo;
+    this._tipo = tipo;
     }
-    getDetalles() 
-{
-    Super.getDetalles();
-    console.log(Empleado.tipo);
-}
+    get tipo(){
+        return this._tipo;
+       }
+       
+       set tipo(nuevotipo){
+         this._tipo = nuevotipo;
+        }
 }
 
 
 
 class Nomina {
-    propiedades = new Array(4);
+   // propiedades = new Array(4);
+   // propiedades=[Empleado.nombre,Empleado.apellido,Empleado.edad,Empleado.tipo]
+   
+   
     
     constructor(nombre, apellido, edad, tipo) {
         this.nombre = nombre;
@@ -39,28 +46,35 @@ class Nomina {
         this.tipo = tipo;
     }
 
-    calcularSueldo()
-    {
-    if (this.tipo = 'C') {
-    dias = 15;
-    let sueldoa = (dias * 500) - ((dias * 500 * 13)/100);
-    console.log(persona.nombre + ' ' + persona.apellido + ' ' + persona.edad +
-    ' quien es espleado de confianza con un sueldo de u$ ' + sueldoa + ' por trabajar ' + dias + ' días') ;
-    } else {
-    dias = 15;
-    let sueldo = (dias * 250) - ((dias * 250 * 10)/100);
-    console.log(persona.nombre + ' ' + persona.apellido + ' ' + persona.edad +
-    ' quien es empleado del Sindicato con un sueldo de u$ ' + sueldo + ' por trabajar ' + dias + ' días') ;
-    }
-    }    
-
+    
+   
 
 }
+function calcularSueldo(Empleado)
+{
+    dias = Math.round(Math.random() * (15- 5) + 5);;
+    if (Empleado.tipo == 'C') {
 
+        let sueldoa = (dias * 500) - ((dias * 500 * 13)/100);
+        console.log(Empleado.nombre + ' ' + Empleado.apellido + ' ' + Empleado.edad +
+        ' quien es empleado de confianza con un sueldo de u$ ' + sueldoa + ' por trabajar ' + dias + ' días') ;
+    } else {
+
+        let sueldo = (dias * 250) - ((dias * 250 * 10)/100);
+        console.log(Empleado.nombre + ' ' + Empleado.apellido + ' ' + Empleado.edad +
+        ' quien es empleado del Sindicato con un sueldo de u$ ' + sueldo + ' por trabajar ' + dias + ' días') ;
+    }
+}    
 
 const persona1 = new Persona('Yeison', 'Jurado', 46);
-persona1.getDetalles();
+const persona2 = new Persona('Yaque', 'Martínez', 45);
+const persona3 = new Persona('carlos', 'Jurado', 21);
+
 
 const empleado1 = new Empleado('Yeison', 'Jurado', 46, 'C');
-//empleado1.getDetalles();
-//empleado1.calcularSueldo();
+const empleado2 = new Empleado('Yaque', 'Martínez', 45, 'C');
+const empleado3 = new Empleado('carlos', 'López', 21, 'S');
+
+calcularSueldo(empleado1);
+calcularSueldo(empleado2);
+calcularSueldo(empleado3);
